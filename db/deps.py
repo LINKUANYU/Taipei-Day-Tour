0425264d4b1cb7d61e73be0caebd5d9335969e4d
kaun <kuan@gmail.com>
2026-04-08 12:50:04 +0800

    rename db 漏掉部分

--- db/deps.py
+++ db/deps.py
@@ -10,7 +10,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")  
-DB_NAME = "travel"
+DB_NAME = os.getenv("DB_NAME")
 
 dbconfig = {
     "host": DB_HOST,
